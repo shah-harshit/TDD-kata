a764acfa52e5d4cc4ca09c216fe5ffc46dc46cbb
Harshit Shah <dinkushah169@gmail.com>
2024-07-04 00:03:07 +0530

    add basic UI and unit tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const addition = "";
+    alert(`Input Value: ${inputValue} \nAddition: ${addition}`);
+    setInputValue("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">Welcome to kata</header>
+      <div className="rules-wrapper">
+        <h4>Instructions:</h4>
+        <p>1. Enter comma separated numbers in the input.</p>
+        <p>2. You can enter any amount of numbers.</p>
+        <p>
+          3. You can enter new lines between numbers instead of commas. For
+          example, "1\n2,3".
+        </p>
+        <p>
+          4. To change the delimiter, the beginning of the string will contain a
+          separate line that looks like this: "//[delimiter]\n[numbers…]". For
+          example, "//;\n1;2" where the delimiter is ";".
+        </p>
+        <p>5. Negative numbers are not supported.</p>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-wrapper">
+          <input
+            data-testid="string-input"
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Enter comma separated numbers (1, 3, 4)"
+          />
+          <button
+            data-testid="add-button"
+            type="submit"
+            disabled={!inputValue.trim()}
+          >
+            Add
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("app.tsx", () => {
+  test("renders appropriate instructions", () => {
+    render(<App />);
+    const instructionsTitle = screen.getByText("Instructions:");
+    const inputInstruction = screen.getByText(
+      "1. Enter comma separated numbers in the input."
+    );
+    const numberCountInstruction = screen.getByText(
+      "2. You can enter any amount of numbers."
+    );
+    const newLineInputInstruction = screen.getByText(
+      '3. You can enter new lines between numbers instead of commas. For example, "1\\n2,3".'
+    );
+    const delimeterInstruction = screen.getByText(
+      '4. To change the delimiter, the beginning of the string will contain a separate line that looks like this: "//[delimiter]\\n[numbers…]". For example, "//;\\n1;2" where the delimiter is ";".'
+    );
+    const negativeNumberInstruction = screen.getByText(
+      "5. Negative numbers are not supported."
+    );
+
+    expect(instructionsTitle).toBeDefined();
+    expect(inputInstruction).toBeDefined();
+    expect(numberCountInstruction).toBeDefined();
+    expect(newLineInputInstruction).toBeDefined();
+    expect(delimeterInstruction).toBeDefined();
+    expect(negativeNumberInstruction).toBeDefined();
+  });
+
+  test("renders input and add button", () => {
+    render(<App />);
+
+    const stringInput = screen.getByTestId("string-input");
+    const addButton = screen.getByTestId("add-button");
+
+    expect(stringInput).toBeDefined();
+    expect(addButton).toBeDefined();
+  });
 });
