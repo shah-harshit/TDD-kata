cef7a5b663934dea7d9333a52d3dfbfa3720c217
Harshit Shah <dinkushah169@gmail.com>
2024-07-03 23:02:34 +0530

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to kata</header>
+    </div>
+  );
+}
+
+export default App;
